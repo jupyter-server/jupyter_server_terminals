b09c1627427fa73dcf20fd64d6a12b216244ff55
Steven Silvester <steven.silvester@ieee.org>
2022-01-29 09:45:08 -0600

    try another method

--- jupyter_server_terminals/test/test_terminal.py
+++ jupyter_server_terminals/test/test_terminal.py
@@ -21,9 +21,6 @@ def terminal_path(tmp_path):
 CULL_TIMEOUT = 10
 CULL_INTERVAL = 3
 
-if os.name == 'nt':
-    CULL_TIMEOUT = 20
-
 
 @pytest.fixture
 def jp_server_config():
@@ -162,7 +159,7 @@ async def test_culling(jp_server_config, jp_fetch, jp_cleanup_subprocesses):
     last_activity = term["last_activity"]
 
     culled = False
-    for i in range(CULL_TIMEOUT + CULL_INTERVAL):
+    for i in range(CULL_TIMEOUT + CULL_INTERVAL * 2):
         try:
             resp = await jp_fetch(
                 "api",
