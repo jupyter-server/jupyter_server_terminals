35450bac875e8f2675e3bb136a088b5588bc69c4
Steven Silvester <steven.silvester@ieee.org>
2023-01-03 08:25:00 -0600

    lint

--- jupyter_server_terminals/terminalmanager.py
+++ jupyter_server_terminals/terminalmanager.py
@@ -8,7 +8,7 @@ from datetime import timedelta
 
 from jupyter_server._tz import isoformat, utcnow
 from jupyter_server.prometheus.metrics import (
-    TERMINAL_CURRENTLY_RUNNING_TOTAL,  # type:ignore[attr-defined]
+    TERMINAL_CURRENTLY_RUNNING_TOTAL, 
 )
 from terminado.management import NamedTermManager
 from tornado import web
