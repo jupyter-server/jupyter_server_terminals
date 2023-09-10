9afdce9669d118caaf30b12d364f3f8103fe076c
Steven Silvester <steven.silvester@ieee.org>
2023-09-10 11:15:01 -0500

    Adopt sp-repo-review (#87)
    * adopt sp-repo-review
    
    * address lint
    
    * handle types
    
    * fix annotation

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,7 +1,7 @@
 .. jupyter_server_terminals documentation master file, created by
    sphinx-quickstart.
    You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+   contain the root ``toctree``` directive.
 
 Welcome to Jupyter Server Terminals documentation!
 ==================================================
--- jupyter_server_terminals/handlers.py
+++ jupyter_server_terminals/handlers.py
@@ -1,10 +1,15 @@
 """Tornado handlers for the terminal emulator."""
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+from __future__ import annotations
+
+import typing as t
+
 from jupyter_server._tz import utcnow
 from jupyter_server.auth.utils import warn_disabled_authorization
 from jupyter_server.base.handlers import JupyterHandler
 from jupyter_server.base.websocket import WebSocketMixin
+from terminado.management import NamedTermManager
 from terminado.websocket import TermSocket as BaseTermSocket
 from tornado import web
 
@@ -18,18 +23,20 @@ class TermSocket(TerminalsMixin, WebSocketMixin, JupyterHandler, BaseTermSocket)
 
     auth_resource = AUTH_RESOURCE
 
-    def initialize(self, name, term_manager, **kwargs):
+    def initialize(  # type:ignore[override]
+        self, name: str, term_manager: NamedTermManager, **kwargs: t.Any
+    ) -> None:
         """Initialize the socket."""
-        BaseTermSocket.initialize(self, term_manager, **kwargs)
-        TerminalsMixin.initialize(self, name)
+        BaseTermSocket.initialize(self, term_manager, **kwargs)  # type:ignore[no-untyped-call]
+        TerminalsMixin.initialize(self, name)  # type:ignore[no-untyped-call]
 
-    def origin_check(self):
+    def origin_check(self, origin: t.Any = None) -> bool:
         """Terminado adds redundant origin_check
         Tornado already calls check_origin, so don't do anything here.
         """
         return True
 
-    def get(self, *args, **kwargs):
+    async def get(self, *args: t.Any, **kwargs: t.Any) -> None:
         """Get the terminal socket."""
         user = self.current_user
 
@@ -39,25 +46,25 @@ class TermSocket(TerminalsMixin, WebSocketMixin, JupyterHandler, BaseTermSocket)
         # authorize the user.
         if not self.authorizer:
             # Warn if an authorizer is unavailable.
-            warn_disabled_authorization()
+            warn_disabled_authorization()  # type:ignore[no-untyped-call]
         elif not self.authorizer.is_authorized(self, user, "execute", self.auth_resource):
             raise web.HTTPError(403)
 
         if args[0] not in self.term_manager.terminals:
             raise web.HTTPError(404)
-        return super().get(*args, **kwargs)
+        await super().get(*args, **kwargs)
 
-    def on_message(self, message):
-        """Handle a socket mesage."""
-        super().on_message(message)
+    async def on_message(self, message: t.Any) -> None:  # type:ignore[override]
+        """Handle a socket message."""
+        await super().on_message(message)
         self._update_activity()
 
-    def write_message(self, message, binary=False):
+    def write_message(self, message: t.Any, binary: bool = False) -> None:  # type:ignore[override]
         """Write a message to the socket."""
         super().write_message(message, binary=binary)
         self._update_activity()
 
-    def _update_activity(self):
+    def _update_activity(self) -> None:
         self.application.settings["terminal_last_activity"] = utcnow()
         # terminal may not be around on deletion/cull
         if self.term_name in self.terminal_manager.terminals:
